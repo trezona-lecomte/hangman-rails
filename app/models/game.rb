047e0613e8b6fa7817d37af301b2fbda3a65ba57
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-24 15:26:03 +1200

    Add game state inclusion validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,8 @@ class Game < ActiveRecord::Base
 
   STATES = %w{ in_progress won lost }
 
+  validates :state, inclusion: { in: STATES, message: "'%{value}' is not a valid state" }
+
   STATES.each do |state|
     define_method("#{state}?") do
       self.state == state
