a35744f03890339508ef6523f03a445c12767566
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-25 08:34:23 +1200

    Add games index action

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  describe "GET index" do
+    before { get :index }
 
+    it "returns 200 OK" do
+      expect(response.status).to be 200
+    end
+
+    it "assigns @games" do
+      game = Game.create
+
+      expect(assigns(:games)).to eq([game])
+    end
+
+    it "renders the index template" do
+      expect(response).to render_template(:index)
+    end
+  end
 end
