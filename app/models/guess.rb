67182b0c76be0afdc7bfbbfc64a97b633912e4f6
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-26 11:25:19 +1200

    Validate guess is part of games alphabet

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,6 +1,6 @@
 class Guess < ActiveRecord::Base
   belongs_to :game
 
-  validates :letter, presence: true
+  validates :letter, presence: true, inclusion: Game::ALPHABET
   validates :game, presence: true
 end
