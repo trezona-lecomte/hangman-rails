3079ef42132c294efae1247dffc2e5f0ad1add3c
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-26 11:37:30 +1200

    Add specs for game - obfuscated letters

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   let(:alphabet) { %w{ a b c d e f g h i j k l m n o p q r s t u v w x y z } }
-  let(:game)     { Game.new }
+  let(:game)     { Game.new(hidden_word: "test") }
 
   describe "#unguessed_letters" do
     context "when no guesses have been made" do
@@ -19,6 +19,25 @@ RSpec.describe Game, type: :model do
         it "doesn't return '#{letter}' when it has been guessed" do
           expect(game.unguessed_letters).to eq(alphabet.reject { |l| l == guessed_letter })
         end
+      end
+    end
+  end
+
+  describe "#obfuscated_letters" do
+    context "when no guesses have been made" do
+      it "returns only mask characters" do
+        expect(game.obfuscated_letters("*")).to eq(["*", "*", "*", "*"])
+      end
+    end
+
+    context "when a correct guess has been made" do
+      before do
+        game.guesses.new(letter: "t")
+        game.guesses.new(letter: "e")
+      end
+
+      it "reveals the letters that were guessed" do
+        expect(game.obfuscated_letters("_")).to eq(["t", "e", "_", "t"])
       end
     end
   end
