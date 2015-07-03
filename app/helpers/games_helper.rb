188bfa8dbf95fc3487eeebdd844e2a686bedf2cd
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-07-03 14:15:33 +1200

    Specify partials explicitly

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,11 +1,11 @@
 module GamesHelper
   def display_status(game)
     if @game.won?
-      render partial: "won"
+      render partial: "games/won"
     elsif @game.lost?
-      render partial: "lost"
+      render partial: "games/lost"
     else
-      render partial: "in_progress"
+      render partial: "games/in_progress"
     end
   end
 end
