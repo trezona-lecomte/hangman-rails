2308f597cda848e3bcfdb3dde1e918e1d4f12898
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-07-03 13:24:02 +1200

    Use options hash on inclusion validation

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,5 +2,5 @@ class Guess < ActiveRecord::Base
   belongs_to :game
 
   validates :game,   presence: true
-  validates :letter, presence: true, inclusion: Game::ALPHABET
+  validates :letter, presence: true, inclusion: { in: Game::ALPHABET }
 end
